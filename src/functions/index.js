310f282437050fb8b53c97ef88a891b019980ab0
Jakushin Cho <weezer970@gmail.com>
2020-01-30 17:05:08 +0900

    add server bot process

--- src/functions/index.js
+++ src/functions/index.js
@@ -22,6 +22,12 @@ exports.next = functions.https.onRequest(async (req, res) => {
   server.use(bodyParser.urlencoded({ extended: false }))
   server.use(bodyParser.json())
 
+  const commands = [
+    "こんにちは",
+    "今何時？",
+    "今日の東京の天気は？"
+  ]
+
   // 履歴
   server.get('/history/list', (req, res) => {
     db.collection('history').orderBy('response_timestamp', 'desc').limit(10).get().then((snapshot) => {
@@ -32,7 +38,7 @@ exports.next = functions.https.onRequest(async (req, res) => {
         let date = new Date(data.response_timestamp.toDate())
         date.setTime(date.getTime() + 1000*60*60*9)
         const dateStr = date.toISOString().split('.')[0]
-        
+
         history.push({
           user_input: data.user_input,
           bot_response: data.bot_response,
@@ -42,6 +48,31 @@ exports.next = functions.https.onRequest(async (req, res) => {
       res.send(JSON.stringify(history));
     })
   });
+
+  // チャット
+  server.post('/chat', (req, res) => {
+    // こんにちは / 今何時？ / 今日の東京の天気は？の命令しか受付しない
+    if (commands.indexOf(req.body.userInput) == -1) {
+      res.status(400).send({err: '入力されたテキストは無効です'})
+      return;
+    }
+
+    // Botの返答
+    let botResponse = ''
+    switch(req.body.userInput) {
+      case commands[0]:
+        botResponse = 'こんにちは。'
+        break;
+      case commands[1]:
+        botResponse = 'HH時MM分です。'
+        break;
+      case commands[2]:
+        botResponse = '晴れです。'
+        break;
+      default:
+        console.log('input is not include any above command.')
+    }
+  })
 
   server.get('*', async (req, res) => {
     console.log('File: ' + req.originalUrl)
